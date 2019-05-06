9da4813721885211c56a9e00c4578cc5778c40e4
Angela Mumba <malaika.mumba@gmail.com>
2019-05-06 21:07:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,26 +4,26 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  array = [a,b,c,d,e,f,g]
+  
 array.sort
 end
 
 def using_reverse(array)
-array = [a,b,c,d,e,f,g]
+
 array.reverse
 end
 
 def using_first(array)
-  array = [a,b,c,d,e,f,g]
+  # array = [a,b,c,d,e,f,g]
 array.first
 end
 
 def using_last(array)
-  array = [a,b,c,d,e,f,g]
+  # array = [a,b,c,d,e,f,g]
 array.last
 end
 
 def using_size(array)
-    array = [a,b,c,d,e,f,g]
+    # array = [a,b,c,d,e,f,g]
 array.size
 end
